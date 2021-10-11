efe9e1cdda067c4423a2681fb257672720f45295
NamoSmith <namosmith2545@gmail.com>
2021-10-11 11:44:59 +0700

    Refactoring.

--- pizza.py
+++ pizza.py
@@ -1,24 +1,40 @@
+from enum import Enum
+
+
+class PizzaSize(Enum):
+    # Enum members written as: name = value
+    small = 120
+    medium = 200
+    large = 280
+
+    def __str__(self):
+        return self.name
+
+
 class Pizza:
     """A pizza with a size and optional toppings."""
 
     def __init__(self, size):
+
         self.size = size
         self.toppings = []
 
-    def getPrice(self):
+    def get_price(self):
         """Price of pizza depends on size and number of toppings."""
-        if self.size == 'small':
-            price = 120 + 20*len(self.toppings)
-        elif self.size == 'medium':
-            price = 200 + 20*len(self.toppings)
-        elif self.size == 'large':
-            price = 280 + 20*len(self.toppings)
-        else:
-            raise ValueError("Unknown pizza size "+self.size)
+        # "Introduce explanatory variable."
+        price = self.size.value + 20*len(self.toppings)
         return price
     
-    def addTopping(self, topping):
+    def add_topping(self, topping):
         """Add a topping to the pizza"""
         if topping not in self.toppings:
             self.toppings.append(topping)
+
+    def __str__(self):
+        description = self.size.name
+        if self.toppings:
+            description += " pizza with " + ", ".join(self.toppings)
+        else:
+            description += " plain cheese pizza"
+        return description
 
